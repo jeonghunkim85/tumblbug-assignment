5a070d136d8deec76262dd5c428a7df0d589b769
jeonghunkim85 <shockshot@naver.com>
2021-01-17 22:35:23 +0900

    import 정리

--- src/main/java/com/tumblbug/assignment/core/applications/port/in/SupportUseCases.java
+++ src/main/java/com/tumblbug/assignment/core/applications/port/in/SupportUseCases.java
@@ -2,8 +2,6 @@ package com.tumblbug.assignment.core.applications.port.in;
 
 import com.tumblbug.assignment.core.domains.Support;
 
-import java.util.UUID;
-
 public interface SupportUseCases {
 
     void doSupport(Support support);
